4265cc91aa9f1e4d610b66b7b8e727c5e56d7109
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-12-24 09:16:53 +0900

    feat: input/output sequence

--- cube2sphere_inv.py
+++ cube2sphere_inv.py
@@ -59,35 +59,54 @@ def fast_equirectangular_from_cubemap(faces_arr, width, height, flip_flags):
 
 def main():
     parser = argparse.ArgumentParser(description='Convert 6 cube face images to equirectangular image (numpy version)')
-    parser.add_argument('front', type=str, help='front face image')
-    parser.add_argument('back', type=str, help='back face image')
-    parser.add_argument('left', type=str, help='left face image')
-    parser.add_argument('right', type=str, help='right face image')
-    parser.add_argument('top', type=str, help='top face image')
-    parser.add_argument('bottom', type=str, help='bottom face image')
+    parser.add_argument('front', type=str, help='front face image (use %%05d for sequence)')
+    parser.add_argument('back', type=str, help='back face image (use %%05d for sequence)')
+    parser.add_argument('left', type=str, help='left face image (use %%05d for sequence)')
+    parser.add_argument('right', type=str, help='right face image (use %%05d for sequence)')
+    parser.add_argument('top', type=str, help='top face image (use %%05d for sequence)')
+    parser.add_argument('bottom', type=str, help='bottom face image (use %%05d for sequence)')
     parser.add_argument('-r', '--resolution', type=int, nargs=2, default=[1024, 512], metavar=('width', 'height'), help='output resolution')
-    parser.add_argument('-o', '--output', type=str, default='out.png', help='output filename')
+    parser.add_argument('-o', '--output', type=str, default='out.png', help='output filename (use %%05d for sequence)')
+    parser.add_argument('--start', type=int, default=None, help='start index for sequence (inclusive)')
+    parser.add_argument('--end', type=int, default=None, help='end index for sequence (inclusive)')
     args = parser.parse_args()
 
-    face_paths = {
-        'front': args.front,
-        'back': args.back,
-        'left': args.left,
-        'right': args.right,
-        'top': args.top,
-        'bottom': args.bottom,
-    }
-    faces = load_faces(face_paths)
-    width, height = args.resolution
-    faces_arr = [faces['front'], faces['back'], faces['right'], faces['left'], faces['top'], faces['bottom']]
-    # 6面のフリップ有無パターンを生成
-    # 正解パターンのみ出力
-    pattern = [0,0,1,1,1,0]  # t0b0r1l1f1k0
-    out_img = fast_equirectangular_from_cubemap(faces_arr, width, height, np.array(pattern))
-    # 最終出力を左右反転
-    out_img = np.ascontiguousarray(out_img[:, ::-1, :])
-    Image.fromarray(out_img).save(args.output)
-    print(f'Saved: {args.output}')
+    if args.start is not None and args.end is not None:
+        for idx in range(args.start, args.end + 1):
+            face_paths = {
+                'front': args.front % idx,
+                'back': args.back % idx,
+                'left': args.left % idx,
+                'right': args.right % idx,
+                'top': args.top % idx,
+                'bottom': args.bottom % idx,
+            }
+            faces = load_faces(face_paths)
+            width, height = args.resolution
+            faces_arr = [faces['front'], faces['back'], faces['right'], faces['left'], faces['top'], faces['bottom']]
+            pattern = [0,0,1,1,1,0]  # t0b0r1l1f1k0
+            out_img = fast_equirectangular_from_cubemap(faces_arr, width, height, np.array(pattern))
+            out_img = np.ascontiguousarray(out_img[:, ::-1, :])
+            output_name = args.output % idx if '%d' in args.output or '%0' in args.output else f"{os.path.splitext(args.output)[0]}_{idx}{os.path.splitext(args.output)[1]}"
+            Image.fromarray(out_img).save(output_name)
+            print(f'Saved: {output_name}')
+    else:
+        face_paths = {
+            'front': args.front,
+            'back': args.back,
+            'left': args.left,
+            'right': args.right,
+            'top': args.top,
+            'bottom': args.bottom,
+        }
+        faces = load_faces(face_paths)
+        width, height = args.resolution
+        faces_arr = [faces['front'], faces['back'], faces['right'], faces['left'], faces['top'], faces['bottom']]
+        pattern = [0,0,1,1,1,0]  # t0b0r1l1f1k0
+        out_img = fast_equirectangular_from_cubemap(faces_arr, width, height, np.array(pattern))
+        out_img = np.ascontiguousarray(out_img[:, ::-1, :])
+        Image.fromarray(out_img).save(args.output)
+        print(f'Saved: {args.output}')
 
 if __name__ == '__main__':
     main()
